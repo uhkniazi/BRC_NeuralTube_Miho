0a3df0347b12c70ba4fc7aa8ae347011715266e3
Umar Niazi <uhkniazi@gmail.com>
2017-08-15 13:28:55 +0100

    pre-trim qc for s107 done.

---
+++ S107/header.R
@@ -0,0 +1,13 @@
+# File: header.R
+# Auth: umar.niazi@kcl.ac.uk
+# DESC: global variables
+# Date: 11/08/2017
+
+
+## variables
+g_pid = 8
+g_did = 14
+gcswd = getwd()
+gcRemoteDir = "/run/user/1000/gvfs/sftp:host=10.202.64.29,user=k1625253/users/k1625253/brc_scratch/Data/ProjectsData/BRC_NeuralTube_Miho/"
+
+p.old = par()
